51b83f210c06a80c74c72e99c6922e47dc091796
Denis <den.dvornikov.88@inbox.ru>
2023-03-20 19:44:30 +0600

    Выполнил задание 1 с помощью функций

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(99,1000); 
+}
+
+int Search(int[] array, int x)
+{
+ for (int i = 0; i < array.Length; i++)
+ {
+  if(array[i]%2==0){
+x+=1;
+  }  
+ }
+ return x;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+int a= 0;
+InputArray(array);
+System.Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+System.Console.WriteLine($"Четных чисел в массиве {Search(array,a)}"); 
